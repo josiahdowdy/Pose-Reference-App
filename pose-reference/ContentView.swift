1a105b73767b44386703eb0bebe60bcdfd8c119a
jojo52 <josiah.dowdy@snhu.edu>
2020-10-16 13:30:35 -0600

    Initial Commit

---
+++ pose-reference/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pose-reference
+//
+//  Created by jo on 2020-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pose-reference/pose_referenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  pose_referenceApp.swift
+//  pose-reference
+//
+//  Created by jo on 2020-10-16.
+//
+
+import SwiftUI
+
+@main
+struct pose_referenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
